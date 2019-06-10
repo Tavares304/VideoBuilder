9078d802c6208144fcb47be07c52a4b753daf8f4
Tavares304 <tavares.he@yahoo.com>
2019-06-10 14:09:16 -0300

    refis a parte do algorithmias e do async

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-	const readline = require('readline-sync')
-	const robots = {
-		text: require('./robots/text.js')
-	}
+const readline = require('readline-sync')
+const robots = {
+	text: require('./robots/text.js')
+}
 
-	function start() {
+function start() {
 	const content = {}
-	
+
 	content.searchTerm = askAndReturnSearchTerm()
 	content.prefix = askAndReturnPrefix()
 
 	robots.text(content)
 
 	function askAndReturnSearchTerm() {
-	 return readline.question('Type a Wikipedia search term: ')
+		return readline.question('Type a Wikipedia search term: ')
 	}
 
 	function askAndReturnPrefix() {
-		const prefix = ['Who is','What is', 'The hystory of']
-		const selectedPrefixIndex = readline.keyInSelect(prefix, 'Choose one option: ')
-		const selectedPrefixText = prefix[selectedPrefixIndex]
-		
+		const prefixes = ['Who is', 'What is', 'The hystory of']
+		const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+		const selectedPrefixText = prefixes[selectedPrefixIndex]
+
 		return selectedPrefixText
 	}
+	
 	console.log(content)
 }
 
